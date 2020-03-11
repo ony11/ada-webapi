8fa4585984120bc1c3fc356261f84f983222c2ed
ony19161 <aman.19161@gmail.com>
2020-03-12 00:35:02 +0600

    Added solution to project
    Modified Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace ada_webapi
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! From " + Environment.MachineName);
                 });
             });
         }
